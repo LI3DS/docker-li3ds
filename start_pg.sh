017c765c5181a51cbac6c3eb1218a1be946d0e86
Éric Lemoine <eric.lemoine@oslandia.com>
2017-08-03 16:21:01 +0200

    Make start_pg.sh use exec to start PostgreSQL

--- start_pg.sh
+++ start_pg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting PostgreSQL..."
-setuser postgres /usr/lib/postgresql/9.5/bin/postgres -D /etc/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf >> /var/log/postgresql.log 2>&1
+exec setuser postgres /usr/lib/postgresql/9.5/bin/postgres -D /etc/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf >> /var/log/postgresql.log 2>&1
