be8715d6a76259e2fee7efb17178f09309bd2cf4
Éric Lemoine <eric.lemoine@oslandia.com>
2017-08-04 09:31:01 +0200

    Change start_api.sh to edit proper file for CPU_COUNT

--- start_api.sh
+++ start_api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i -e 's/{CPU_COUNT}/'"$CPU_COUNT"'/g' /api-li3ds/conf/api_li3ds.yml
+sed -i -e 's/{CPU_COUNT}/'"$CPU_COUNT"'/g' /api-li3ds/conf/api_li3ds.uwsgi.yml
 sed -i -e 's/{API_KEY}/'"$API_KEY"'/g' /api-li3ds/conf/api_li3ds.yml
 exec uwsgi --yaml /api-li3ds/conf/api_li3ds.uwsgi.yml
